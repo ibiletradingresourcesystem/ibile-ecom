72d6fc69911c262cc8d54fac24e37c1c77873cc3
Ayoola <47798700+casstimmy@users.noreply.github.com>
2026-08-05 12:47:23 +0100

    improve on overall styling

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "image-bucket-admin.s3.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "st-micheals-hub.s3.eu-north-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.s3.*.amazonaws.com",
+      },
     ],
   },
 };
